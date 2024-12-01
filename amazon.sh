206a6f783e4b47e69b21e49b810e80a6bb995397
Rakesh Reddy <rakeshreddymadayarla2@gmail.com>
2024-12-02 00:24:35 +0530

    Create amazon.sh

---
+++ amazon.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+yum install httpd git -y
+systemctl start httpd
+systemctl status httpd
+cd /var/www/html
+git clone https://github.com/rakeshreddyRR/AMAZON-.git
+mv AMAZON-/* .
+tail -100f /var/log/httpd/access_log
