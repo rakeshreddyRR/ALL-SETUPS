17d16ec8c9c9b2b36c8017b14f52c413abeb82f0
Rakesh Reddy <rakeshreddymadayarla2@gmail.com>
2024-12-03 22:09:34 +0530

    Update amazon.sh

--- amazon.sh
+++ amazon.sh
@@ -3,6 +3,6 @@ yum install httpd git -y
 systemctl start httpd
 systemctl status httpd
 cd /var/www/html
-git clone https://github.com/rakeshreddyRR/amazon.git
-mv amazon/* .
+git clone https://github.com/rakeshreddyRR/amazon-clone-raw.git
+mv amazon-clone-raw/* .
 tail -100f /var/log/httpd/access_log
